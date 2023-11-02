346c0b3392fcaac008ac76b443efd900f6e5bd3a
ZaMan <21971457+Zaman8@users.noreply.github.com>
2023-11-02 13:40:09 -0400

    Darkmode (#5)
    * remove color css elements from lanyon.scss
    
    * scss files for dark theme, still lots of issues
    
    * unstyled working button for changing themes
    
    * Styled theme button and tweaks to colors
    
    * themes.scss takes advantage of mixins for DRY
    
    * visual-lanyon.scss uses mixins for DRY
    
    * save user theme using localstorage
    
    * Styled theme toggle button
    
    * darken images in dark mode
    
    * set initial color theme based on user system pref
    
    * fix for nested link style
    
    * fixed sidebar toggle colors not updating

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,27 @@
   var toggle = document.querySelector('.sidebar-toggle');
   var sidebar = document.querySelector('#sidebar');
   var checkbox = document.querySelector('#sidebar-checkbox');
+  var themeSwitcher = document.getElementById('theme-switcher');
 
+  //set page dark/light mode to last specified by user or use prefers-colors-theme
+  var settheme = 'light-theme';
+  //determine if user set theme themselves
+  if(localStorage.getItem('theme') === null) {
+    //if no user specified check if they have a prefers-color-scheme of dark
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      settheme = 'dark-theme';
+    } 
+  } else {
+    //if user has specified a theme use that
+    settheme = localStorage.getItem('theme');
+  }
+
+  //set page theme
+  document.body.setAttribute('class', settheme);
+
+
+  //add listener event to sidebar toggle
+  //uncheck checkbox when clicking outside sidebar
   document.addEventListener('click', function(e) {
     var target = e.target;
 
@@ -12,4 +32,20 @@
 
     checkbox.checked = false;
   }, false);
+
+  //add button functionality to themeswitcher
+  themeSwitcher.onclick = function() {
+    if(document.body.getAttribute('class') === 'light-theme') {
+      //set theme
+      document.body.setAttribute('class', 'dark-theme');
+      //save new preference
+      localStorage.setItem('theme', 'dark-theme');
+    }
+    else {
+      //set theme
+      document.body.setAttribute('class', 'light-theme');
+      //save new preference
+      localStorage.setItem('theme', 'light-theme');
+    }
+  }
 })(document);
